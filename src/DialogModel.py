1226ceccfba113a6dbd8d7920dbaab7c37e65314
Sylvain Bouchard <sylvain.bouchard@live.ca>
2018-07-18 18:23:59 -0400

    Start of implementation of bi-directional LSTM in Keras

---
+++ src/DialogModel.py
@@ -0,0 +1,16 @@
+class DialogModel(object):
+    '''
+    Main interface class for the model. It implements training based on early stopping and testing
+    and interactive interfaces.
+    '''
+
+    def __init__(self, config=None):
+        print("To do")
+
+    def init_network(self):
+
+        print("Initialising network...")
+
+        # Initialise network model
+
+    
